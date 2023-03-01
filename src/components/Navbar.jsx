4d474b844b42db6c578bce7463ee4521ea80f9db
Your Name <you@example.com>
2023-03-01 13:02:00 +0530

    search error message done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,12 @@ function Navbar() {
     return (
         <div>
             <div className=' flex justify-between items-center bg-[#282828] p-2'>
-                <h1 className=' font-bold text-xl text-white'>NEWS <span className=" text-green-600 font-bold">24/7</span></h1>
+                <h1 className=' font-bold text-xl text-white'>NEWS <span className=" text-green-500 font-bold">24/7</span></h1>
                 <button onClick={changetheme}>Dark mode</button>
             </div>
             <hr />
-            <div className=' sm:p-2 bg-[#282828] h-8 overflow-x-auto'>
-                <ul className=' flex gap-5 pb-1 font-semibold sm:mx-3 cursor-pointer text-white '>
+            <div className=' sm:p-2 bg-[#282828] h-10 overflow-x-auto mb-4'>
+                <ul className=' flex gap-5 font-semibold sm:mx-3 cursor-pointer text-white '>
                     {
                         arr.map((item, index) => {
                             return <li className='hover:border-b-2' key={index} id={`${index}`} onClick={(e) => {
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -14,30 +14,40 @@ function News() {
         loadnews(topic);
         setSpinner(false);
     }, [topic])
+
+    const handleclick = (e) => {
+        e.preventDefault();
+        changetopic(temp)
+    }
     return (
         <div>
             <div className=' flex flex-col items-center sm:justify-between sm:items-center sm:px-3 sm:flex-row m-2'>
-                <h1 className='text-3xl font-bold mb-2 text-white'>TOP NEWS Of {topic}</h1>
+                <p className='text-3xl font-bold mb-5 text-white break-all w-full sm:w-1/2 sm:mb-2'>TOP NEWS Of {topic.toUpperCase()}</p>
                 <div>
-                    <form>
+                    <form onSubmit={handleclick}>
                         <input type="text" required className=' border-2 rounded p-1 focus:outline-none' placeholder='Search Catageries' onChange={(e) => {
                             settemp(e.target.value);
                         }} />
-                        <button className='bg-green-700 text-white p-1 px-2 rounded mx-2 font-bold hover:bg-green-900'
-                            onClick={() => {
-                                changetopic(temp)
-                            }}>Search</button>
+                        <button type="submit" className='bg-green-600 text-white p-1 px-2 rounded mx-2 font-bold hover:bg-green-900'
+                        >Search</button>
                     </form>
                 </div>
             </div>
-            {spinner ? <PulseLoader size={8} color="#ffffff" /> :
-                (data?.error) ? <div className='text-white'> hii </div> : <div className=' grid grid-cols-1 gap-4 m-4 mt-7 mb-5 sm:grid-cols-3 lg:grid-cols-4 p-2'>
-                    {
-                        data?.map((item, index) => {
-                            return <NewsCard title={item.title} description={item.description} author={item.author} urlToImage={item.urlToImage} url={item.url} key={index} id={index} publishedAt={item.publishedAt} />
-                        })
-                    }
-                </div>}
+            <div className='flex items-center justify-center'>
+                {spinner ? <PulseLoader size={8} color="#ffffff" loading={spinner} /> :
+                    (data?.error) ? <div className='text-white flex flex-col m-3 mt-10 mb-4 p-3 bg-[#282828] rounded justify-center sm:w-1/2'>
+                        <h1 className=' text-2xl font-bold text-green-500'>News not found</h1>
+                        <p className=' text-md'>Sorry, we couldn't find news for topic you are looking for.....</p>
+                    </div> :
+
+                        <div className=' grid grid-cols-1 gap-4 m-4 mt-7 mb-5 sm:grid-cols-3 lg:grid-cols-4 p-2'>
+                            {
+                                data?.map((item, index) => {
+                                    return <NewsCard title={item.title} description={item.description} author={item.author} urlToImage={item.urlToImage} url={item.url} key={index} id={index} publishedAt={item.publishedAt} />
+                                })
+                            }
+                        </div>}
+            </div>
         </div>
     )
 }
